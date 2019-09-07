c8b892d056bddf992b13f33c0ec5e4053584ffe0
Vladislav Setchin <vlad@setchinfc.com.au>
2019-09-07 22:16:33 +1000

    Implementing ExceptionLayoutRenderer

---
+++ vscnlog.tests/ExceptionLayoutRendererTester.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+using NLog;
+
+namespace vscnlog.tests
+{
+    public class ExceptionLayoutRendererTester
+    {
+        private static Logger _log = LogManager.GetCurrentClassLogger();
+
+        [Fact]
+        public void RendererFormatErrorMessage()
+        {
+            try
+            {
+                FunctionWithException("Exception!!!");
+            }
+            catch (System.Exception ex)
+            {
+                _log.Error(ex, "Error Happend!");
+                Assert.True(true);
+            }
+        }
+
+        private static void FunctionWithException(string value)
+        {
+            try
+            {
+                value.Substring(100, 100);
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception("Function had exception!", ex);
+            }
+        }
+    }
+}
---
+++ vscnlog/ExceptionLayoutRenderer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using NLog;
+using NLog.Config;
+using NLog.LayoutRenderers;
+
+namespace VSC.NLog
+{
+    [LayoutRenderer("ExceptionRenderer")]
+    [ThreadAgnostic]
+    public class ExceptionLayoutRenderer : LayoutRenderer
+    {
+        protected ExceptionLayoutRenderer()
+        {
+        }
+
+        [DefaultValue("")]
+        public string Indent { get; set; }
+
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            var ex = logEvent.Exception;
+            if(ex != null)
+            {
+                this.FormatErrorMessage(builder, ex, this.Indent);
+            }
+        }
+
+        private void FormatErrorMessage(StringBuilder builder, Exception exception, string indent = "")
+        {
+            string newLine = "\n";
+            string stars = new string('*', 80);
+            builder.Append(indent + stars + newLine);
+            builder.Append(indent + exception.GetType().Name + ": \"" + exception.Message + "\"" + newLine);
+            builder.Append(indent + new string('-', 80) + newLine);
+
+            try
+            {
+                if (exception.InnerException != null)
+                {
+                    builder.Append(indent + "InnerException:" + newLine);
+                    FormatErrorMessage(builder, exception.InnerException, indent + "   ");
+                }
+
+                if (exception.StackTrace != null)
+                {
+                    foreach (string line in exception.StackTrace.Split(new string[] { " at " }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (string.IsNullOrEmpty(line.Trim())) continue;
+                        string[] parts;
+                        parts = line.Trim().Split(new string[] { " in " }, StringSplitOptions.RemoveEmptyEntries);
+                        string class_info = parts[0];
+                        if (parts.Length == 2)
+                        {
+                            parts = parts[1].Trim().Split(new string[] { "line" }, StringSplitOptions.RemoveEmptyEntries);
+                            string src_file = parts[0];
+                            int line_nr = int.Parse(parts[1]);
+                            builder.Append(indent + "  " +src_file.TrimEnd(':') + "(" + line_nr + ",1):   " + class_info + newLine);
+                        }
+                        else
+                        {
+                            builder.Append(indent + "  " + class_info + newLine);
+                        }
+                    }
+                }
+
+                builder.Append(indent + stars + newLine);
+            }
+            catch (Exception ex)
+            {
+                builder.Append(ex.ToString());
+            }
+        }
+    }
+}
